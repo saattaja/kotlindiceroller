cfa938e983d82795b12543a2d2d01572b47f2dbd
saattaja <janita.elisa@outlook.com>
2023-09-13 17:36:31 +0300

    kaksi noppaa

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -41,8 +41,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
-    var result by remember { mutableStateOf(1) }
-    val imageResource = when (result){
+    var result1 by remember { mutableStateOf(1) }
+    val imageResource = when (result1){
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
         3 -> R.drawable.dice_3
@@ -50,14 +50,31 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
+    var result2 by remember { mutableStateOf(1) }
+    val imageResource2 = when (result2){
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    fun CreateRandoms(){
+        result1 = (1..6).random()
+        result2 = (1..6).random()
+    }
 Column(modifier = modifier,
     horizontalAlignment = Alignment.CenterHorizontally){
     Image(
         painter = painterResource(id=imageResource),
-        contentDescription = result.toString()
+        contentDescription = result1.toString()
+    )
+    Image(
+        painter = painterResource(id=imageResource2),
+        contentDescription = result2.toString()
     )
     Spacer(modifier = Modifier.height(16.dp))
-    Button(onClick = { result = (1..6).random() }) {
+    Button(onClick = { CreateRandoms() }) {
         Text(stringResource(R.string.roll))
     }
 
